a8e26f9e84ab2b3be565517e63e661477b2a9d17
SlawomirKowalczyk <s.kowalczyk24@gmail.com>
2019-02-16 20:45:34 +0100

    small and simple refactoring

---
+++ src/SalaryCalculator/Main.java
@@ -0,0 +1,12 @@
+package SalaryCalculator;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SalaryCalculator app = new SalaryCalculator();
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
+
+}
--- src/SalaryCalculator/SalaryCalculator.java
+++ src/SalaryCalculator/SalaryCalculator.java
@@ -40,30 +40,46 @@ import javax.swing.UnsupportedLookAndFeelException; */
  */
 public class SalaryCalculator extends JFrame implements ActionListener {
 
-    JButton bCalculate;
-    JLabel lTitle, lGrossSalary, lTaxYear, lWorkplace,
-            lE, lR, lC, lZ, lZalPom, lNettSalary,
-            lShowGrossSalary, lShowE, lShowR, lShowC, lShowZ, lShowZalPom, lShowNettSalary,
-            linformation1, linformation2;
-    JMenuBar menuBar;
-    JMenu menuFile, menuView, menuHelp;
-    JMenuItem mOpen, mSave, mExit,
+    private JButton bCalculate;
+    private JLabel lTitle;
+    private JLabel lGrossSalary;
+    private JLabel lTaxYear;
+    private JLabel lWorkplace;
+    private JLabel lE;
+    private JLabel lR;
+    private JLabel lC;
+    private JLabel lZ;
+    private JLabel lZalPom;
+    private JLabel lNettSalary;
+    private JLabel lShowGrossSalary;
+    private JLabel lShowE;
+    private JLabel lShowR;
+    private JLabel lShowC;
+    private JLabel lShowZ;
+    private JLabel lShowZalPom;
+    private JLabel lShowNettSalary;
+    private JLabel linformation2;
+    private JMenu menuFile;
+    private JMenu menuView;
+    private JMenuItem mOpen, mSave, mExit,
             mMetal, mNimbus, mWindows,
             mAbout;
-    JTextField tBrutto;
-    JCheckBox ChYesNo;
-    BigDecimal brutto, netto, E, R, C, PNFZ, Z, ZP, Po, Zal, ZalPom, z1, KW, KUP;
-    double z2, z3, z4, z5;
+    private JTextField tBrutto;
+    private JCheckBox ChYesNo;
+    private BigDecimal brutto, netto, E, R, C, PNFZ, Z, ZP, Po, Zal, ZalPom, z1, KW, KUP;
+    private double z2, z3, z4, z5;
 
     public SalaryCalculator() {
-        setSize(500, 550);
-        setTitle("Kalkulator Wynagrodzenia");
-        setLayout(null);
+        initWindowProperties();
 
-        menuBar = new JMenuBar();
+        initButons();
+    }
+
+    private void initButons() {
+        JMenuBar menuBar = new JMenuBar();
         menuFile = new JMenu("Plik");
         menuView = new JMenu("Widok");
-        menuHelp = new JMenu("Pomoc");
+        JMenu menuHelp = new JMenu("Pomoc");
         setJMenuBar(menuBar);
 
         menuBar.add(menuFile);
@@ -185,7 +201,7 @@ public class SalaryCalculator extends JFrame implements ActionListener {
         lShowNettSalary.setBounds(220, 390, 200, 20);
         add(lShowNettSalary);
 
-        linformation1 = new JLabel("Wskazówka: Możesz zapisać otrzymane wyniki obliczeń, np. do pliku *.txt.");
+        JLabel linformation1 = new JLabel("Wskazówka: Możesz zapisać otrzymane wyniki obliczeń, np. do pliku *.txt.");
         linformation1.setBounds(10, 420, 450, 20);
         add(linformation1);
 
@@ -194,47 +210,118 @@ public class SalaryCalculator extends JFrame implements ActionListener {
         add(linformation2);
     }
 
+    private void initWindowProperties() {
+        setSize(500, 550);
+        setTitle("Kalkulator Wynagrodzenia");
+        setLayout(null);
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         Object z = e.getSource();
         if (z == mOpen) {
-            JFileChooser fc = new JFileChooser();
-            if (fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-                try {
-                    Desktop.getDesktop().edit(fc.getSelectedFile());
-                } catch (IOException ex) {
-                    Logger.getLogger(SalaryCalculator.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
+            openFileWindow();
         }
 
         if (z == mSave) {
-            JFileChooser okno = new JFileChooser();
-            if (okno.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
-                File plik = okno.getSelectedFile();
-                //JOptionPane.showMessageDialog(null, "Wybraliśmy plik to: " + plik);
-                try {
-                    PrintWriter save = new PrintWriter(plik, "UTF-8");
-                    save.println("Kwota brutto = " + brutto + " PLN");
-                    save.println("Ubezpieczenie emerytalne = " + E + " PLN");
-                    save.println("Ubezpieczenie rentowe = " + R + " PLN");
-                    save.println("Ubezpieczenie chorobowe = " + C + " PLN");
-                    save.println("Ubezpieczenie zdrowotne = " + Z + " PLN");
-                    save.println("Zaliczka na podatek dochodowy = " + ZalPom + " PLN");
-                    save.println("Wynagrodzenie netto = " + netto + " PLN");
-                    save.println();
-                    save.close();
-                } catch (FileNotFoundException ex) {
-                    Logger.getLogger(SalaryCalculator.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (UnsupportedEncodingException ex) {
-                    Logger.getLogger(SalaryCalculator.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
+            saveResultWindow();
         }
 
         if (z == mExit) {
             dispose();
         }
+        setView(z);
+        if (z == mAbout) {
+            JOptionPane.showMessageDialog(null, "Kalkulator wynagrodzenia pracownika"
+                    + "\nstyczeń 2017"
+                    + "\nwersia 1.0\n\n"
+                    + "autor: Sławomir Kowalczyk",
+                    "O Programie",
+                    JOptionPane.INFORMATION_MESSAGE);
+        }
+        if (ChYesNo.isSelected()) {
+            KUP = new BigDecimal("111.25");
+        } else if (!ChYesNo.isSelected()) {
+            KUP = new BigDecimal("135.63");
+        }
+        if (z == bCalculate) {
+
+            calculations();
+        }
+    }
+
+    private void calculations() {
+        try {
+            brutto = new BigDecimal(tBrutto.getText());
+            brutto = brutto.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+            E = brutto.multiply(new BigDecimal("0.0976"));
+            E = E.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+
+            R = brutto.multiply(new BigDecimal("0.015"));
+            R = R.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+
+            C = brutto.multiply(new BigDecimal("0.0245"));
+            C = C.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+            PNFZ = ((brutto.subtract(E)).subtract(R)).subtract(C);
+
+            Z = PNFZ.multiply(new BigDecimal("0.09"));
+            Z = Z.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+
+            ZP = PNFZ.multiply(new BigDecimal("0.0775"));
+            ZP = ZP.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+
+            Po = PNFZ.subtract(KUP);
+            Po = Po.setScale(0, BigDecimal.ROUND_HALF_EVEN);
+
+            Zal = Po.multiply(new BigDecimal("0.18"));
+            Zal = Zal.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+
+            KW = new BigDecimal("46.335");
+            ZalPom = (Zal.subtract(ZP)).subtract(KW);
+            ZalPom = ZalPom.setScale(0, BigDecimal.ROUND_HALF_EVEN);
+
+            /**
+             * Obliczenia dotyczące składki zdrowotnej i zaliczki na podatek
+             * dochodowy zgodne z art 83 ust. 1 i 2 ustawy z dn.27.08.2004
+             * o świadczeniach opieki zdrowotnej finansowanych ze środków
+             * publicznych, z późn. zm.
+             *
+             * @param z1..z5 - zmienne pomocnicze
+             */
+
+            z1 = Zal.subtract(KW);
+            z1 = z1.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+
+            z2 = Double.parseDouble(Z.toString());
+            z3 = Double.parseDouble(z1.toString());
+            if (z2 > z3) {
+                Z = z1;
+            }
+            z4 = Double.parseDouble(Z.toString());
+            if (z4 < 0) {
+                Z = new BigDecimal("0");
+            }
+            z5 = Double.parseDouble(ZalPom.toString());
+            if (z5 < 0) {
+                ZalPom = new BigDecimal("0");
+            }
+
+            netto = (PNFZ.subtract(Z)).subtract(ZalPom);
+            netto = netto.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+
+            lShowGrossSalary.setText(brutto.toString() + " PLN");
+            lShowE.setText(E.toString() + " PLN");
+            lShowR.setText(R.toString() + " PLN");
+            lShowC.setText(C.toString() + " PLN");
+            lShowZ.setText(Z.toString() + " PLN");
+            lShowZalPom.setText(ZalPom.toString() + " PLN");
+            lShowNettSalary.setText(netto.toString() + " PLN");
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Wpisz prawidłowe dane", "Błąd", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+
+    private void setView(Object z) {
         if (z == mMetal) {
             try {
                 UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
@@ -275,95 +362,40 @@ public class SalaryCalculator extends JFrame implements ActionListener {
             }
             SwingUtilities.updateComponentTreeUI(this);
         }
-        if (z == mAbout) {
-            JOptionPane.showMessageDialog(null, "Kalkulator wynagrodzenia pracownika"
-                    + "\nstyczeń 2017"
-                    + "\nwersia 1.0\n\n"
-                    + "autor: Sławomir Kowalczyk",
-                    "O Programie",
-                    JOptionPane.INFORMATION_MESSAGE);
-        }
-        if (ChYesNo.isSelected()) {
-            KUP = new BigDecimal("111.25");
-        } else if (!ChYesNo.isSelected()) {
-            KUP = new BigDecimal("135.63");
-        }
-        if (z == bCalculate) {
+    }
 
+    private void saveResultWindow() {
+        JFileChooser okno = new JFileChooser();
+        if (okno.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+            File plik = okno.getSelectedFile();
+            //JOptionPane.showMessageDialog(null, "Wybraliśmy plik to: " + plik);
             try {
-                brutto = new BigDecimal(tBrutto.getText());
-                brutto = brutto.setScale(2, BigDecimal.ROUND_HALF_EVEN);
-                E = brutto.multiply(new BigDecimal("0.0976"));
-                E = E.setScale(2, BigDecimal.ROUND_HALF_EVEN);
-
-                R = brutto.multiply(new BigDecimal("0.015"));
-                R = R.setScale(2, BigDecimal.ROUND_HALF_EVEN);
-
-                C = brutto.multiply(new BigDecimal("0.0245"));
-                C = C.setScale(2, BigDecimal.ROUND_HALF_EVEN);
-                PNFZ = ((brutto.subtract(E)).subtract(R)).subtract(C);
-
-                Z = PNFZ.multiply(new BigDecimal("0.09"));
-                Z = Z.setScale(2, BigDecimal.ROUND_HALF_EVEN);
-
-                ZP = PNFZ.multiply(new BigDecimal("0.0775"));
-                ZP = ZP.setScale(2, BigDecimal.ROUND_HALF_EVEN);
-
-                Po = PNFZ.subtract(KUP);
-                Po = Po.setScale(0, BigDecimal.ROUND_HALF_EVEN);
-
-                Zal = Po.multiply(new BigDecimal("0.18"));
-                Zal = Zal.setScale(2, BigDecimal.ROUND_HALF_EVEN);
-
-                KW = new BigDecimal("46.335");
-                ZalPom = (Zal.subtract(ZP)).subtract(KW);
-                ZalPom = ZalPom.setScale(0, BigDecimal.ROUND_HALF_EVEN);
-                
-                /**
-                 * Obliczenia dotyczące składki zdrowotnej i zaliczki na podatek
-                 * dochodowy zgodne z art 83 ust. 1 i 2 ustawy z dn.27.08.2004
-                 * o świadczeniach opieki zdrowotnej finansowanych ze środków
-                 * publicznych, z późn. zm.
-                 *
-                 * @param z1..z5 - zmienne pomocnicze
-                 */
-                
-                z1 = Zal.subtract(KW);
-                z1 = z1.setScale(2, BigDecimal.ROUND_HALF_EVEN);
-
-                z2 = Double.parseDouble(Z.toString());
-                z3 = Double.parseDouble(z1.toString());
-                if (z2 > z3) {
-                    Z = z1;
-                }
-                z4 = Double.parseDouble(Z.toString());
-                if (z4 < 0) {
-                    Z = new BigDecimal("0");
-                }
-                z5 = Double.parseDouble(ZalPom.toString());
-                if (z5 < 0) {
-                    ZalPom = new BigDecimal("0");
-                }
-
-                netto = (PNFZ.subtract(Z)).subtract(ZalPom);
-                netto = netto.setScale(2, BigDecimal.ROUND_HALF_EVEN);
-
-                lShowGrossSalary.setText(brutto.toString() + " PLN");
-                lShowE.setText(E.toString() + " PLN");
-                lShowR.setText(R.toString() + " PLN");
-                lShowC.setText(C.toString() + " PLN");
-                lShowZ.setText(Z.toString() + " PLN");
-                lShowZalPom.setText(ZalPom.toString() + " PLN");
-                lShowNettSalary.setText(netto.toString() + " PLN");
-            } catch (Exception ex) {
-                JOptionPane.showMessageDialog(null, "Wpisz prawidłowe dane", "Błąd", JOptionPane.INFORMATION_MESSAGE);
+                PrintWriter save = new PrintWriter(plik, "UTF-8");
+                save.println("Kwota brutto = " + brutto + " PLN");
+                save.println("Ubezpieczenie emerytalne = " + E + " PLN");
+                save.println("Ubezpieczenie rentowe = " + R + " PLN");
+                save.println("Ubezpieczenie chorobowe = " + C + " PLN");
+                save.println("Ubezpieczenie zdrowotne = " + Z + " PLN");
+                save.println("Zaliczka na podatek dochodowy = " + ZalPom + " PLN");
+                save.println("Wynagrodzenie netto = " + netto + " PLN");
+                save.println();
+                save.close();
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(SalaryCalculator.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (UnsupportedEncodingException ex) {
+                Logger.getLogger(SalaryCalculator.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
 
-    public static void main(String[] args) {
-        SalaryCalculator app = new SalaryCalculator();
-        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        app.setVisible(true);
+    private void openFileWindow() {
+        JFileChooser fc = new JFileChooser();
+        if (fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            try {
+                Desktop.getDesktop().edit(fc.getSelectedFile());
+            } catch (IOException ex) {
+                Logger.getLogger(SalaryCalculator.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 }
